4ea30b33998e85e8fe84807f48c51657f400d787
Resutemon1 <104874711+Resutemon1@users.noreply.github.com>
2025-06-02 03:28:56 +0000

    banco de dados integrado

--- FirebaseConfig.js
+++ FirebaseConfig.js
@@ -9,24 +9,14 @@ import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY ,
-  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBXlBjGxBqqV6X7jYNbkOTHwu6yHJ60I5I",
+  authDomain: "appfinancas-cc674.firebaseapp.com",
+  projectId: "appfinancas-cc674",
+  storageBucket: "appfinancas-cc674.firebasestorage.app",
+  messagingSenderId: "997422524157",
+  appId: "1:997422524157:web:2e6f06fa4dbddc86a7c094"
 };
 
-/* 
-const firebaseConfig = {
-  apiKey: "AIzaSyCrJN3rdJvPxJl0u3xiCgvw-0wao73LWVo",
-  authDomain: "projeto-mobile-7ebc0.firebaseapp.com",
-  projectId: "projeto-mobile-7ebc0",
-  storageBucket: "projeto-mobile-7ebc0.firebasestorage.app",
-  messagingSenderId: "17456492053",
-  appId: "1:17456492053:web:8d117ae2733bdf9cac281c"
-};  
-*/
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
